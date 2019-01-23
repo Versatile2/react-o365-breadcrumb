eb8adb71a7a3892b2970c80bd2131c37e8408dc8
Versatile2 <Alllexx2@gmail.com>
2019-01-23 16:23:22 +0100

    sss

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,60 @@
-import BreadCrumb from './components/BreadCrumb';
-export default BreadCrumb;
+import * as React from 'react';
+import { IBreadcrumbItem, Breadcrumb } from 'office-ui-fabric-react/lib';
+import ISiteBreadcrumbProps from './components/Interface/ISiteBreadcrumbProps';
+import ISiteBreadcrumbState from './components/Interface/ISiteBreadcrumbState';
+import NavigationNodeUtils from './utils/NavigationNodeUtils';
+import INavigationNode from './utils/INavigationNode';
+import { IBreadCrumbProps } from './components/IBreadCrumbProps';
+
+
+export default class BreadCrumb extends React.Component<
+    ISiteBreadcrumbProps,
+    ISiteBreadcrumbState
+    > {
+    private linkItems: IBreadcrumbItem[];
+    private currentRelativeurl: string;
+    private nodeUtils = new NavigationNodeUtils();
+
+    constructor(props: ISiteBreadcrumbProps) {
+        super(props);
+        this.linkItems = [];
+        this.currentRelativeurl = this.props.context.pageContext.legacyPageContext.serverRequestPath;
+    }
+    public componentWillMount() {
+        const currentNav = this.props.context.pageContext.legacyPageContext.navigationInfo.quickLaunch;
+        const navigationNodes: INavigationNode[] = this.nodeUtils.getNodePath(this.currentRelativeurl, currentNav).reverse();
+        if (this.props.elementbefore) {
+            this.pushArrayInLinkItems(this.props.elementbefore);
+        }
+        if (!this.nodeUtils.homeRegex.test(this.currentRelativeurl)) {
+            this.linkItems.push({
+                key: currentNav[0].Id,
+                text: currentNav[0].Title,
+                href: currentNav[0].Url
+            });
+        }
+        navigationNodes.forEach(element => {
+            this.linkItems.push({
+                key: element.Id,
+                text: element.Title,
+                href: element.Url
+            });
+        });
+
+        this.linkItems[this.linkItems.length - 1].isCurrentItem = true;
+        console.log(this.linkItems);
+    }
+
+
+    private pushArrayInLinkItems(array: IBreadcrumbItem[]) {
+        if (this.props.elementbefore) {
+            array.forEach(element => {
+                this.linkItems.push(element);
+            });
+        }
+    }
+
+    public render(): React.ReactElement<IBreadCrumbProps> {
+        return <Breadcrumb items={this.linkItems} />;
+    }
+}

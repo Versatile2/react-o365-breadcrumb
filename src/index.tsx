be303ce8ffbde52a59b86963709f5724a6922432
Versatile2 <Alllexx2@gmail.com>
2019-01-23 16:06:41 +0100

    mmm

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,2 @@
-/**
- * @class ExampleComponent
- */
-
-import * as React from 'react'
-
-import styles from './styles.css'
 import BreadCrumb from './components/BreadCrumb';
-import { IBreadcrumbItem } from 'office-ui-fabric-react/lib';
-
-export type Props = {
-  context: any;
-  elementbefore?: IBreadcrumbItem[];
-}
-
-export default class ExampleComponent extends React.Component<Props> {
-  render() {
-    const {
-      context
-    } = this.props
-
-    return (
-      <BreadCrumb context={context}></BreadCrumb>
-    )
-  }
-}
+export default BreadCrumb;
